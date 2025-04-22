fecc38ab515a71304ecc31afec9d5d20588cf1f9
Udomomo <udomomo@gmail.com>
2025-04-22 21:03:16 +0900

    add logging

--- client/src/main/kotlin/com/udomomo/grpcclient/controller/PeopleRestController.kt
+++ client/src/main/kotlin/com/udomomo/grpcclient/controller/PeopleRestController.kt
@@ -2,6 +2,7 @@ package com.udomomo.grpcclient.controller
 
 import com.udomomo.grpcclient.client.PeopleServiceGrpcClient
 import com.udomomo.grpcclient.model.People
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController
 class PeopleRestController(
     private val peopleServiceGrpcClient: PeopleServiceGrpcClient
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @GetMapping("")
     fun list(): List<People> {
+        logger.info("Start listing people")
         return peopleServiceGrpcClient.listPeople()
     }
 
     @PostMapping("")
     fun create(@RequestBody request: PeopleRequest) {
+        logger.info("Start creating people: name=${request.name}, age=${request.age}")
         peopleServiceGrpcClient.createPeople(request.name, request.age)
     }
 }
--- server/src/main/kotlin/com/udomomo/grpcserver/grpcservice/PeopleServiceImpl.kt
+++ server/src/main/kotlin/com/udomomo/grpcserver/grpcservice/PeopleServiceImpl.kt
@@ -3,6 +3,7 @@ package com.udomomo.grpcserver.grpcservice
 import com.google.protobuf.Empty
 import com.udomomo.grpcserver.repository.PeopleRepository
 import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
 import people.PeopleOuterClass
 import people.PeopleServiceGrpcKt
 
@@ -10,9 +11,13 @@ import people.PeopleServiceGrpcKt
 class PeopleServiceImpl(
     private val peopleRepository: PeopleRepository
 ): PeopleServiceGrpcKt.PeopleServiceCoroutineImplBase() {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override suspend fun createPeople(
         request: PeopleOuterClass.CreatePeopleRequest
     ): PeopleOuterClass.CreatePeopleResponse {
+        logger.info("Receive createPeople request | name=${request.name}, age=${request.age}")
+
         peopleRepository.save(request.name, request.age)
 
         return PeopleOuterClass.CreatePeopleResponse.newBuilder()
@@ -23,6 +28,8 @@ class PeopleServiceImpl(
     override suspend fun listPeople(
         request: PeopleOuterClass.ListPeopleRequest
     ): PeopleOuterClass.ListPeopleResponse {
+        logger.info("Receive listPeople request")
+
         val peoples = peopleRepository.list().map {
             PeopleOuterClass.People.newBuilder()
                 .setId(it.id)
